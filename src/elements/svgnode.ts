c3616de4577611dccdf9463ed4539a84bc6c6351
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2023-04-28 13:18:22 +0200

    Changed side-detection system

--- src/elements/svgnode.ts
+++ src/elements/svgnode.ts
@@ -20,14 +20,6 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-export const COLLISION = {
-    NONE: Symbol("NONE"),
-    TOP: Symbol("TOP"),
-    RIGHT: Symbol("RIGHT"),
-    BOTTOM: Symbol("BOTTOM"),
-    LEFT: Symbol("LEFT"),
-};
-
 interface ISVGNodeParams {
     selector?: string;
     domNode?: HTMLElement;
@@ -62,6 +54,10 @@ export class SVGNode {
         return this.node.setAttribute(attrName, value);
     }
 
+    /**
+     * Check collision using AABB collision.
+     * @param svg The element to check collision against.
+     */
     collidesWith(svg: SVGNode) {
         return (
             this.x + this.width >= svg.x && // left-edge
@@ -71,22 +67,111 @@ export class SVGNode {
         );
     }
 
-    collidesWithWhichEdge(svg: SVGNode) {
-        if (!this.collidesWith(svg)) {
-            return { edge: COLLISION.NONE };
-        }
+    /**
+     * Check if a point is inside a polygon defined by edges.
+     * Each edge has to be represented by a pair of starting X,Y and ending X,Y.
+     * @param edges An array of edges.
+     * @param xp An x-coordinate of a point.
+     * @param yp An y-coordinate of a point.
+     * @returns A boolean that tells if a point is inside.
+     */
+    static isPointInside(edges: number[][], xp: number, yp: number) {
+        let count = 0;
+        edges.forEach((edge) => {
+            const [x1, y1, x2, y2] = edge;
+            if (yp < y1 != yp < y2 && xp < x1 + ((yp - y1) / (y2 - y1)) * (x2 - x1)) count++;
+        });
+        return count % 2 == 1;
+    }
 
-        if (this.x + this.width === svg.x) {
-            return { edge: COLLISION.LEFT };
-        } else if (this.x === svg.x + svg.width) {
-            return { edge: COLLISION.RIGHT };
-        } else if (this.y + this.height === svg.y) {
-            return { edge: COLLISION.TOP };
-        } else if (this.y === svg.y + svg.height) {
-            return { edge: COLLISION.BOTTOM };
-        } else {
-            return { edge: COLLISION.NONE };
-        }
+    get topEdge() {
+        return [this.x, this.y, this.x1, this.y];
+    }
+
+    get rightEdge() {
+        return [this.x1, this.y, this.x1, this.y1];
+    }
+
+    get bottomEdge() {
+        return [this.x, this.y1, this.x1, this.y1];
+    }
+
+    get leftEdge() {
+        return [this.x, this.y, this.x, this.y1];
+    }
+
+    /**
+     * Returns the edges of the node. This assumes that the node is a rectangle.
+     */
+    getEdges() {
+        return [this.topEdge, this.rightEdge, this.bottomEdge, this.leftEdge];
+    }
+
+    /**
+     * Returns the edges of top triangular overlap area.
+     */
+    get topOverlapAreaEdges() {
+        const triangle_left = [this.x, this.y, this.midX, this.midY];
+        const triangle_right = [this.x1, this.y, this.midX, this.midY];
+        return [this.topEdge, triangle_right, triangle_left];
+    }
+
+    /**
+     * Returns the edges of right triangular overlap area.
+     */
+    get rightOverlapAreaEdges() {
+        const triangle_top = [this.x1, this.y, this.midX, this.midY];
+        const triangle_bottom = [this.x1, this.y1, this.midX, this.midY];
+        return [this.rightEdge, triangle_bottom, triangle_top];
+    }
+
+    /**
+     * Returns the edges of bottom triangular overlap area.
+     */
+    get bottomOverlapAreaEdges() {
+        const triangle_left = [this.x, this.y1, this.midX, this.midY];
+        const triangle_right = [this.x1, this.y1, this.midX, this.midY];
+        return [this.bottomEdge, triangle_right, triangle_left];
+    }
+
+    /**
+     * Returns the edges of left triangular overlap area.
+     */
+    get leftOverlapAreaEdges() {
+        const triangle_top = [this.x, this.y, this.midX, this.midY];
+        const triangle_bottom = [this.x, this.y1, this.midX, this.midY];
+        return [this.leftEdge, triangle_top, triangle_bottom];
+    }
+
+    overlapTop(svg: SVGNode) {
+        return SVGNode.isPointInside(this.topOverlapAreaEdges, svg.midX, svg.y1);
+    }
+
+    /**
+     * Determine the overlap from the right side with the SVG.
+     * The overlap is calculated until the center.
+     * @param svg
+     */
+    overlapRight(svg: SVGNode) {
+        return SVGNode.isPointInside(this.rightOverlapAreaEdges, svg.x - 1, svg.midY);
+    }
+
+    overlapBottom(svg: SVGNode) {
+        return SVGNode.isPointInside(this.bottomOverlapAreaEdges, svg.midX, svg.y - 1);
+    }
+
+    overlapLeft(svg: SVGNode) {
+        return SVGNode.isPointInside(this.leftOverlapAreaEdges, svg.x1, svg.midY);
+    }
+
+    overlap(svg: SVGNode) {
+        const overlap = {
+            top: this.overlapTop(svg),
+            bottom: this.overlapBottom(svg),
+            right: this.overlapRight(svg),
+            left: this.overlapLeft(svg),
+        };
+        return overlap;
     }
 
     get x() {
@@ -97,12 +182,28 @@ export class SVGNode {
         this.set("x", String(value));
     }
 
+    get x1() {
+        return this.x + this.width;
+    }
+
+    get y1() {
+        return this.y + this.height;
+    }
+
     get y() {
         return this.get("y");
     }
 
     set y(value: number) {
         this.set("y", String(value));
+    }
+
+    get midX() {
+        return this.x + this.width / 2;
+    }
+
+    get midY() {
+        return this.y + this.height / 2;
     }
 
     get width() {
